90758f3f2a639a567b104a2a41dd62704a11d526
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2023-11-09 15:58:17 -0600

    Done

--- app/utils.py
+++ app/utils.py
@@ -1,22 +1,20 @@
-# app/utils.py
-
 import requests
 
 def fetch_pokemon_info(pokemon_name):
-    # Placeholder URL for the actual PokeAPI call: https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}
-    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}'
+    response = requests.get(url)
 
-    response = requests.get(url.lower())
-    
     if response.ok:
         data = response.json()
+        abilities = [data['abilities'][0]['ability']['name'], data['abilities'][1]['ability']['name']]
+        
         pokemon_info = {
             'name': data['name'],
             'hp': data['stats'][0]['base_stat'],
             'attack': data['stats'][1]['base_stat'],
             'defense': data['stats'][2]['base_stat'],
             'sprite': data['sprites']['front_default'],
-            'abilities': [data['abilities'][0]['ability']['name'], data['abilities'][1]['ability']['name']]
+            'abilities': ', '.join(abilities)
         }
 
         return pokemon_info
